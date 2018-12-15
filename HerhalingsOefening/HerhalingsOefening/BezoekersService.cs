5aee7d76e2db915d5fdd460e2707663789016d89
Daan <daan.deschaumes@student.howest.be>
2018-12-15 19:01:52 +0100

    GetBezoekers Function

--- HerhalingsOefening/HerhalingsOefening/BezoekersService.cs
+++ HerhalingsOefening/HerhalingsOefening/BezoekersService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.IO;
 using System.Threading.Tasks;
+using HerhalingsOefening.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
@@ -19,26 +20,74 @@ namespace HerhalingsOefening
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "days")] HttpRequest req,
             ILogger log)
         {
-            List<string> days = new List<string>();
-           
-
-            using (SqlConnection connection = new SqlConnection(Environment.GetEnvironmentVariable("ConnectionString")))
+            try
             {
-                await connection.OpenAsync();
-                using (SqlCommand command = new SqlCommand())
-                {
-                    command.Connection = connection;
-                    string sql = "SELECT DISTINCT DagVanDeWeek FROM TijdReeksen ";
-                    command.CommandText = sql;
+                List<string> days = new List<string>();
 
-                    SqlDataReader reader = await command.ExecuteReaderAsync();
-                    while (reader.Read())
+
+                using (SqlConnection connection = new SqlConnection(Environment.GetEnvironmentVariable("ConnectionString")))
+                {
+                    await connection.OpenAsync();
+                    using (SqlCommand command = new SqlCommand())
                     {
-                        days.Add(reader["DagVanDeWeek"].ToString());
+                        command.Connection = connection;
+                        string sql = "SELECT DISTINCT DagVanDeWeek FROM TijdReeksen ";
+                        command.CommandText = sql;
+
+                        SqlDataReader reader = await command.ExecuteReaderAsync();
+                        while (reader.Read())
+                        {
+                            days.Add(reader["DagVanDeWeek"].ToString());
+                        }
                     }
                 }
+                return new OkObjectResult(days);
             }
-            return new OkObjectResult(days);
+            catch(Exception ex)
+            {
+                return new StatusCodeResult(500);
+            }
+           
         }
+
+        [FunctionName("GetBezoekers")]
+        public static async Task<IActionResult> GetBezoekers(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "bezoekers/{day}")]
+            HttpRequest req, string day, ILogger log)
+        {
+            List<Visit> visits = new List<Visit>();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(Environment.GetEnvironmentVariable("ConnectionString")))
+                {
+                    await connection.OpenAsync();
+
+                    using (SqlCommand command = new SqlCommand())
+                    {
+                        command.Connection = connection;
+                        string sql = "SELECT * FROM TijdReeksen WHERE DagVanDeWeek = @day";
+                        command.CommandText = sql;
+                        command.Parameters.AddWithValue("@day", day);
+                        SqlDataReader reader = await command.ExecuteReaderAsync();
+                        while (reader.Read())
+                        {
+                            visits.Add(new Visit()
+                            {
+                                AantalBezoekers = int.Parse(reader["AantalBezoekers"].ToString()),
+                                Dag = reader["DagVanDeWeek"].ToString(),
+                                Tijdstip = int.Parse(reader["TijdstipDag"].ToString())
+                            });
+                        }
+                    }
+                }
+                return new OkObjectResult(visits);
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex, "GetBezoekersOpDag");
+                return new StatusCodeResult(500);
+            }
+        }
+
     }
 }
---
+++ HerhalingsOefening/HerhalingsOefening/Model/Visit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HerhalingsOefening.Model
+{
+    public class Visit
+    {
+        public int Tijdstip { get; set; }
+        public int AantalBezoekers { get; set; }
+        public string Dag { get; set; }
+    }
+}
